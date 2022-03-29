4d5840dfdb02c301fcdfa3ae74b2e9ad20babe7f
Piotr Kalitka <kalitkapiotr@gmail.com>
2022-03-29 18:20:43 +0200

    Configure Eslint

--- js/script.js
+++ js/script.js
@@ -2,46 +2,46 @@
 
 
 function titleClickHandler(event){
-		event.preventDefault();
-		const clickedElement = this;
-    console.log('Link was clicked!');
+  event.preventDefault();
+  const clickedElement = this;
+  console.log('Link was clicked!');
     
   
-    /* [DONE] remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
 
-    const activeLinks = document.querySelectorAll('.titles a.active');
+  const activeLinks = document.querySelectorAll('.titles a.active');
 
-    for(let activeLink of activeLinks){
-      activeLink.classList.remove('active');
-    }
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
   
-    /*[DONE]  add class 'active' to the clicked link */
+  /*[DONE]  add class 'active' to the clicked link */
 
-		// console.log('clickedElement:', clickedElement);
-		clickedElement.classList.add('active');
+  // console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active');
 		
   
-    /* [DONE] remove class 'active' from all articles */
+  /* [DONE] remove class 'active' from all articles */
 
-		const activeArticles = document.querySelectorAll('.posts article.active');
+  const activeArticles = document.querySelectorAll('.posts article.active');
 
-    for(let activeArticle of activeArticles){
-      activeArticle.classList.remove('active');
-    }
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
 
-    /* [DONE] get 'href' attribute from the clicked link */
+  /* [DONE] get 'href' attribute from the clicked link */
 
-		const articleSelector = clickedElement.getAttribute('href');
-		console.log(articleSelector);
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector);
   
-    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
 
-		const targetArticle = document.querySelector(articleSelector);
-		console.log(targetArticle);
+  const targetArticle = document.querySelector(articleSelector);
+  console.log(targetArticle);
   
-    /* [DONE]  add class 'active' to the correct article */
+  /* [DONE]  add class 'active' to the correct article */
 
-		targetArticle.classList.add('active');
+  targetArticle.classList.add('active');
 }
 
 
@@ -55,7 +55,7 @@ function generateTitleLinks(){
 
   /* [DONE]  remove contents of titleList */
   const titleList=document.querySelector(optTitleListSelector);
-  titleList.innerHTML="";
+  titleList.innerHTML='';
 
   /* [DONE] for each article */
   const articles = document.querySelectorAll(optArticleSelector);
